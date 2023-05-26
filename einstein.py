d5f97713d2235956688712cda7e849fdc1ca1245
Leo <chidalukizitonwauwa@gmail.com>
2023-05-27 00:37:00 +0200

    Moved files to new folder

--- einstein.py
+++
@@ -1,6 +0,0 @@
-#prompt the user to enter a value for m
-m = int(input ("Enter the mass in kilograms: "))
-#c is a constant having a value of 300000000
-c = 300000000
-E = m * (c**2)
-print ("E =", E, "Joules")
--- playback.py
+++
@@ -1,5 +0,0 @@
-#prompt the user to enter a sentence
-text = input ("Enter a sentence: ")
-#format the sentence to replace all white spaces with "..."
-text = text.replace(" ", "...")
-print(text)
